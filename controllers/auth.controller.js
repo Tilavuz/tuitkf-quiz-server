6da1a8d6379301a208d685207f47d2a9e9e7dccf
Tilavuz <tilav777@gmail.com>
2024-07-13 13:48:35 +0500

    change some errors

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,22 +1,24 @@
 const { generateToken } = require("../helpers/generate-token");
 const User = require("../models/user.model");
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 
 const login = async (req, res) => {
   try {
     const { phone, password } = req.body;
-    if (!phone || !password) res.status(400).json({ message: "Kirish uchun malumot yetarli emas!" });
+    if (!phone || !password)
+      return res.status(400).json({ message: "Kirish uchun malumot yetarli emas!" });
 
-    let user = await User.findOne({ phone })
-    if(!user) res.status(404).json({ message: 'Foydalanuvchi topilmadi!' })
+    let user = await User.findOne({ phone });
+    if (!user)
+      return res.status(404).json({ message: "Foydalanuvchi topilmadi!" });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) res.status(401).json({ message: 'Parolda xatolik bor!' })
-    
-    const token = generateToken({ _id: user._id })
+    if (!isMatch) return res.status(401).json({ message: "Parolda xatolik bor!" });
 
-    user = await User.findById(user._id).select('-password')
-    res.json({ token, user })
+    const token = generateToken({ _id: user._id });
+
+    user = await User.findById(user._id).select("-password");
+    res.json({ token, user });
   } catch (error) {
     res.status(500).json(error.message);
   }
@@ -26,31 +28,30 @@ const register = async (req, res) => {
   try {
     const { phone, password, name } = req.body;
     if (!phone || !password || !name)
-      res
+      return res
         .status(400)
         .json({ message: "Ro'yhatdan o'tish uchun malumot yetarli emas!" });
 
     let user = await User.findOne({ phone });
-    if (user) res.status(409).json({ message: "Telefon raqam bazada mavjut!" });
-
+    if (user) return  res.status(409).json({ message: "Telefon raqam bazada mavjut!" });
 
     user = await User.create({ phone, password, name });
     const token = generateToken({ _id: user._id });
     user = await User.findById(user._id).select("-password");
 
-    res.status(201).json({user, token});
+    res.status(201).json({ user, token });
   } catch (error) {
     res.json({ message: error.message });
   }
 };
 
 const getUser = async (req, res) => {
-    try {
-        const user = await User.findById(req.user._id).select('-password')
-        res.status(200).json(user)
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-}
+  try {
+    const user = await User.findById(req.user._id).select("-password");
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 module.exports = { register, login, getUser };
